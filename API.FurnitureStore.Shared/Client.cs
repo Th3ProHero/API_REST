9b29c967038dc95a705aba530f03aeccbc6867c0
Mauricio Bautista Flores <107211188+Th3ProHero@users.noreply.github.com>
2024-06-03 22:05:12 -0600

    data access layer

---
+++ API.FurnitureStore.Shared/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.FurnitureStore.Shared
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime birthDate { get; set; }
+        public string phone { get; set; }   
+        public string adress { get; set; }
+    }
+}
