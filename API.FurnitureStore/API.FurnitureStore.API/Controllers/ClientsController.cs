666a2f30aed199083dc3f9ba9697ec462e79eda9
Th3ProHero <bautista.flores.mauricio2@gmail.com>
2024-06-05 22:06:42 -0600

    Added Product Categories

--- API.FurnitureStore/API.FurnitureStore.API/Controllers/ClientsController.cs
+++ API.FurnitureStore/API.FurnitureStore.API/Controllers/ClientsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.FurnitureStore.Data;
+using API.FurnitureStore.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.FurnitureStore.API.Controllers
 {
@@ -7,5 +10,55 @@ namespace API.FurnitureStore.API.Controllers
     [ApiController]
     public class ClientsController : ControllerBase
     {
+        private readonly APIFurnitureStoreContext _context;
+
+        public ClientsController(APIFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Client>> Get()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (client == null) return NotFound();
+
+            return Ok(client);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Client client)
+        {
+            _context.Clients.Add(client);
+            _context.SaveChangesAsync();
+            return CreatedAtAction("Post", client.Id, client);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Client client)
+        {
+            if (client == null) return NotFound();
+
+            _context.Clients.Remove(client);
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+
     }
 }
